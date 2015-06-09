4326c12a00ebc0c4e708eeb1656f65a0f6d16be0
rschwager-hsph <rschwager-hsph@localhost>
2015-06-09 10:38:30 -0400

    fix: use the bowtie2_path option if you have it

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='knead-datalib',
+    version='0.0.1',
+    description='',
+    packages=['knead_datalib'],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ],
+    scripts=[os.path.join(here, "knead_data.py")],
+    entry_points = {
+        "distutils.commands": [
+            "trimmomatic = knead_datalib.util:DownloadTrimmomaticCommand"
+        ]
+    }
+)
+
